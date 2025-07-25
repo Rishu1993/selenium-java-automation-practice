4b041cc7e5c78188e685003ab19f1578d4e30487
Rishu Bajaj <rishu.bajaj@ibm.com>
2025-07-25 22:58:12 +0530

    addedtocart

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,7 +1,10 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProductsPage {
 	
@@ -12,13 +15,34 @@ WebDriver driver;
 		this.driver=driver;
 	}
 	
-	By productsLink=By.xpath("//a[text()=' Products']");
+	public By productsLink=By.xpath("//a[text()=' Products']");
+	
+	By listOfProds=By.xpath("//div[@class='productinfo text-center']//p");
+	By verifcation=By.xpath("//p[text()='Your product has been added to cart.']");
 	
 	public void clickProductLink()
 	{
 		driver.findElement(productsLink).click();
 	}
 
+	public List<WebElement> allProds()
+	{
+		List<WebElement> listOfProd=driver.findElements(listOfProds);
+		return listOfProd;
+	}
 	
+	public void addCart(String productName)
+	{
+	     String dynamicXPath = "//p[text()='" + productName + "']/parent::div[@class='productinfo text-center']//a";
+
+		driver.findElement(By.xpath(dynamicXPath)).click();
+		
+	}
+	public boolean verificationMsg()
+	{
+		boolean isDisplayed=driver.findElement(verifcation).isDisplayed();
+		return isDisplayed;
+		
+	}
 
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.apache.hc.core5.util.Asserts;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -15,23 +18,44 @@ import pages.ProductsPage;
 public class ProductsTest extends BaseTest {
 	
 	@Test
-	public void  clickProductsTest()
+	public void  clickProductsTest() throws InterruptedException
 	{
 		
 		LoginPagePF login=new LoginPagePF(driver);
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
 		login.enterEmail("rishua1993@gmail.com");
 		login.enterPassword("Bajajr@01");
 		login.clickLogin();
+		String product="Stylish Dress";
 	
 		ProductsPage prod=new ProductsPage(driver);
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(prod.productsLink));
+
 		prod.clickProductLink();
 		
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		
 		
 		wait.until(ExpectedConditions.titleIs("Automation Exercise - All Products"));
 		
 		Assert.assertEquals(driver.getTitle(), "Automation Exercise - All Products", "Title not same");
+		Thread.sleep(10000);
+
+		List<WebElement> lists=prod.allProds();
 		
+		for(WebElement e:lists)
+		{
+
+			if(e.getText().equals(product))
+			{
+Thread.sleep(3000);
+				prod.addCart(product);
+				System.out.println("Added to cart");
+				Thread.sleep(3000);
+			boolean isDisplayed=	prod.verificationMsg();
+				Assert.assertTrue(isDisplayed);
+			}
+		}
 		
 
 
